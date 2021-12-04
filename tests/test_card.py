f55a65043f9e72847e6f103df03d386056cb4dbe
Jason Ward <jason.louard.ward@gmail.com>
2021-12-04 18:30:28 -0500

    Added tests for cards.

---
+++ tests/test_card.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+from card import Card
+
+
+class CardTestCase(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.card = Card(number=1, suit='a')
+
+    def test_str_is_same_as_repr(self):
+        self.assertEqual(str(self.card), repr(self.card))
+
+    def test_face_handles_numbers(self):
+        self.assertEqual(self.card.face, 'A')
+
+    def test_value_handles_face_cards(self):
+        self.card.number = 15
+        self.assertEqual(self.card.value, 10)
+        self.card.number = 7
+        self.assertEqual(self.card.value, 7)
+
+    def test_card_can_be_check_for_less_than(self):
+        card = Card(number=2, suit='a')
+        assert card > self.card
